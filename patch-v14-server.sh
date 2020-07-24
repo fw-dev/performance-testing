ae057d7dcf84cef932c87cc316d52a43f778f4e9
John Clayton <johnc@filewave.com>
2020-07-24 23:05:31 +0200

    Syntax

--- patch-v14-server.sh
+++ patch-v14-server.sh
@@ -5,8 +5,7 @@ echo "Patching $supervisord_conf"
 if [ -f "$supervisord_conf" ]; then
     sed -i "s/apache\/logs\/\*_log\*\"/apache\/logs\/error_log\*\"/g" $supervisord_conf
     grep "error_log\*" $supervisord_conf 
-    R=$?
-    if [ $R -ne 0 ]; then
+    if [ $? -ne 0 ]; then
         echo "Failed to patch $supervisord_conf; error_log isnt found in the file..."
         exit 1
     fi
